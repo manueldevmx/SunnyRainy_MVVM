50def3d0bcb42646ec50db652dd4f06308fb2b48
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-03-30 19:22:21 -0600

    Update LocationPermissions.kt

--- app/src/main/java/com/manueldevmx/sunnyrainy_mvvm/core/LocalPermissions.kt
+++ app/src/main/java/com/manueldevmx/sunnyrainy_mvvm/core/LocalPermissions.kt
@@ -1,2 +1,19 @@
 package com.manueldevmx.sunnyrainy_mvvm.core
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
+
+private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+
+fun checkPermissions(context: Context) =
+    ActivityCompat.checkSelfPermission(context,
+    Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PermissionChecker.PERMISSION_GRANTED
+
+fun startLocationPermissionRequest(activity: Activity){
+    ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+        REQUEST_PERMISSIONS_REQUEST_CODE)
+}
